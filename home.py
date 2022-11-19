5c3c86dacc149a17153e783ee8f17fe300d1f231
pcadjk <98069008+pcadjk@users.noreply.github.com>
2022-11-19 11:57:37 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
